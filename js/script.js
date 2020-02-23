ff687205672fc7fe3e42822d628cb204b7a6e3d2
BudiSalah <budi.salah@outlook.com>
2020-02-23 13:32:09 +0200

    Update skills on index and chart labels on script.js

--- js/script.js
+++ js/script.js
@@ -8,11 +8,9 @@ function chart() {
     var ctx = document.getElementById('myChart').getContext('2d'),
         data = {
             datasets: [{
-                data: [90, 70, 35, 85, 65],
+                data: [80, 15, 5],
                 backgroundColor: [
                     'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 64, 159)',
                     'rgb(255, 205, 86)',
                     'rgb(86, 205, 255)'
                 ]
@@ -20,11 +18,9 @@ function chart() {
         
             // These labels appear in the legend and in the tooltips when hovering different arcs
             labels: [
-                "HTML, CSS, Sass, Bootstrap, Git",
-                "Angular, Ionic, SQL",
-                "C, Python, Linux, Php",
-                "Javascript, Jquery, React JS",
-                "Adobe CC"
+                "Web Development",
+                "Database",
+                "Graphic Design"
             ]
         };
 
